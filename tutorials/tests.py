edc4261037cba531e592356e8f0d2a605ecb1ffb
primecoach619 <primecoach619@gmail.com>
2023-08-08 15:28:22 -0700

    first commit

---
+++ tutorials/tests.py
@@ -0,0 +1,59 @@
+from django.test import TestCase
+from django.urls import reverse
+from tutorials.models import Tutorial
+import pytest
+
+# Create your tests here.
+def test_homepage_access():
+    url = reverse('home')
+    assert url == "/"
+
+#Using fixtures to test Django Models
+# - We will want to write more integration tests that require creating a tutorial object.
+# - We can convert our test_create_tutorial() function into a fixture to do this.
+# - Update the test_create_tutorial() function as follows:
+@pytest.fixture
+def new_tutorial(db):
+    tutorial = Tutorial.objects.create(
+        title='Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    return tutorial
+
+def test_search_tutorials(new_tutorial):
+    assert Tutorial.objects.filter(title='Pytest').exists()
+
+def test_update_tutorial(new_tutorial):
+    new_tutorial.title = 'Pytest-Django'
+    new_tutorial.save()
+    assert Tutorial.objects.filter(title='Pytest-Django').exists()
+
+@pytest.fixture
+def another_tutorial(db):
+    tutorial = Tutorial.objects.create(
+        title='More-Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    return tutorial
+
+def test_compare_tutorials(new_tutorial, another_tutorial):
+    assert new_tutorial.pk != another_tutorial.pk
+
+#Integration test: user login
+# - Django has a built-in user model and authentication system (see https://docs.djangoproject.com/en/3.2/topics/auth/).  
+# - It is possible to implement our own custom authentication system, but for this example, we will use Django's.
+# - We will add a basic integration test to test user login, using a fixture to first create a user to log in. 
+@pytest.fixture
+def test_user(db, django_user_model):
+    django_user_model.objects.create_user(
+        username="test_username", password="test_password")
+    return "test_username", "test_password"   # this returns a tuple
+
+def test_login_user(client, test_user):
+    test_username, test_password = test_user  # this unpacks the tuple
+    login_result = client.login(username=test_username, password=test_password)
+    assert login_result == True
